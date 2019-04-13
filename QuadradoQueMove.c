d2a554ce18cda6668cf7a4df750faa9ca7fc5e16
CMinoves <cminoves26@gmail.com>
2019-04-13 15:05:17 -0300

    v1.0 funções expecíficas para desenhar quadrados e início da rolagem de tela

--- QuadradoQueMove.c
+++ QuadradoQueMove.c
@@ -7,15 +7,56 @@ int X = 45;
 #define ALTURA_DO_MUNDO 100
 
 int keyboard[256];
-
-struct objetos{
+float aspectratio = 0.7692;
+typedef struct {
   double posx;
   double posy;
   double tamx;
   double tamy;
   int existe;
-}anzol,fundo;
+}objetos;
 float W;
+objetos coisas[100];
+objetos anzol;
+objetos fundo;
+//coisas[1] = fundo;
+int contobj = 1;
+float yfundo = 0;
+void desenhaQuadrado(void){
+  glClear(GL_COLOR_BUFFER_BIT);
+//desenhando o fundo cabuloso
+glColor3f(1, 0, 0);
+  glPushMatrix();
+    glTranslatef(0,yfundo,0);
+    glBegin(GL_TRIANGLE_FAN);
+      glVertex3f(-(coisas[0].tamx)/2, -(coisas[0].tamy)/2, 0);
+      glColor3f(0, 1, 0);
+      glVertex3f((coisas[0].tamx)/2, -(coisas[0].tamy)/2, 0);
+      glColor3f(0, 0, 1);
+      glVertex3f((coisas[0].tamx)/2, (coisas[0].tamy)/2, 0);
+      glColor3f(1, 1, 0);
+      glVertex3f(-(coisas[0].tamx)/2, (coisas[0].tamy)/2, 0);
+    glEnd();
+  glPopMatrix();
+  //fim do desenho do fundo
+  // Começa a usar a cor verde
+  glColor3f(0, 1, 0);
+  for(int x = 1;x<contobj;x++){
+  if(coisas[x].existe){
+    glPushMatrix();
+      glTranslatef(coisas[x].posx,coisas[x].posy,0);
+      glBegin(GL_TRIANGLE_FAN);
+        glVertex3f(-(coisas[x].tamx)/2, -(coisas[x].tamy)/2, 0);
+        glVertex3f((coisas[x].tamx)/2, -(coisas[x].tamy)/2, 0);
+        glVertex3f((coisas[x].tamx)/2, (coisas[x].tamy)/2, 0);
+        glVertex3f(-(coisas[x].tamx)/2, (coisas[x].tamy)/2, 0);
+      glEnd();
+    glPopMatrix();
+  }
+}
+  glutSwapBuffers();
+}
+/*
 void desenhaCena(void)
 {
     // Limpa a tela (com a cor definida por glClearColor(R,G,B)) para que
@@ -42,57 +83,41 @@ void desenhaCena(void)
     // Diz ao OpenGL para colocar o que desenhamos na tela
     glutSwapBuffers();
 }
-
+*/
 // Inicia algumas variáveis de estado
 void inicializa(void)
 {
     // cor para limpar a tela
     glClearColor(1, 1, 1, 0);      // branco
     //criando o anzol
-
+    anzol.posx = 0;
+    anzol.posy = 72;
+    anzol.tamx = 30;
+    anzol.tamy = 30;
+    anzol.existe = 1;
+    coisas[1] = anzol;
+    contobj ++;
+    fundo.posx = 0;
+    fundo.posy = 0;
+    fundo.tamx = 500;
+    fundo.tamy = 5000;
+    fundo.existe = 1;
+    coisas[0] = fundo;
 }
 
 // Callback de redimensionamento
 void redimensiona(int w, int h)
 {
-    /*   glViewport(0, 0, w, h);
-       glMatrixMode(GL_PROJECTION);
-       glLoadIdentity();
-       glOrtho(0, 100, 0, 100, -1, 1);
-       glMatrixMode(GL_MODELVIEW);
-       glLoadIdentity(); */
        W = w;
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(-w/2, w/2, -h/2, h/2, -1, 1);
     glViewport(0, 0, w, h);
-/*
-    float razaoAspectoJanela = ((float)w)/h;
-    float razaoAspectoMundo = ((float) LARGURA_DO_MUNDO)/ ALTURA_DO_MUNDO;
-    // se a janela está menos larga do que o mundo (16:9)...
-    if (razaoAspectoJanela < razaoAspectoMundo)
-    {
-        // vamos colocar barras verticais (acima e abaixo)
-        float hViewport = w / razaoAspectoMundo;
-        float yViewport = (h - hViewport)/2;
-        glViewport(0, yViewport, w, hViewport);
-    }
-    // se a janela está mais larga (achatada) do que o mundo (16:9)...
-    else if (razaoAspectoJanela > razaoAspectoMundo)
-    {
-        // vamos colocar barras horizontais (esquerda e direita)
-        float wViewport = ((float)h) * razaoAspectoMundo;
-        float xViewport = (w - wViewport)/2;
-        glViewport(xViewport, 0, wViewport, h);
-    }
-    else
-    {
-        glViewport(0, 0, w, h);
-    }
-    */
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+    int w2 = h*aspectratio;
+  //  glutReshapeWindow(500, 500);
 }
 
 // Callback de evento de teclado
@@ -135,26 +160,28 @@ void upteclado(unsigned char key, int x, int y)
 }
 void atualiza()
 {
-
-    desenhaCena();
+  //movimento do anzol
+  if(keyboard['a']){
+    coisas[1].posx-=10;
+  }
+  if(keyboard['d']){
+    coisas[1].posx+=10;
+  }
+  //desenha o anzol
+  printf("%f\n",anzol.posx);
+ yfundo +=10;
+  //  desenhaCena();
+  //  glutSwapBuffers();
     glutPostRedisplay();
     glutTimerFunc(25, atualiza, 0);
 
-}
 
+}
 // Rotina principal
 int main(int argc, char **argv)
 {
 
 
-    anzol.posx = 0;
-    anzol.posy = 72;
-    anzol.tamx = 30;
-    anzol.tamy = 30;
-    anzol.existe = 1;
-
-    fundo.posx = 50;
-    fundo.tamx = 50;
     //fundo.posy =
     // Acordando o GLUT
     glutInit(&argc, argv);
@@ -165,7 +192,7 @@ int main(int argc, char **argv)
 
     // Configuração inicial da janela do GLUT
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(400, 400);
+    glutInitWindowSize(500, 650);
     glutInitWindowPosition(100, 100);
 
     // Abre a janela
@@ -173,12 +200,13 @@ int main(int argc, char **argv)
     inicializa();
 
     // Registra callbacks para alguns eventos
-    glutDisplayFunc(desenhaCena);
+    glutDisplayFunc(desenhaQuadrado);
     glutReshapeFunc(redimensiona);
     glutKeyboardFunc(teclado);
     glutKeyboardUpFunc(upteclado);
     inicializa();
     glutTimerFunc(0, atualiza, 0);
+  //
 
     // Entra em loop e nunca sai
     glutMainLoop();
